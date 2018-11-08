ec3c1f10e8beb3206bd423c49dede94dc5cd3641
Mellon <melker.lonnehed@elev.ga.lbs.se>
2018-11-08 14:53:58 +0100

    Monoshuto Dogatari (Final Version)
    Added startup effects and additional balancing; The final version.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,11 +8,12 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb2D;
 
     private float x;
-    private float y;
+    private float y;   
 
     //private bool isBoosting;
     //private bool isReversing;
 
+    private bool startup = true;
     private bool isFiring;
     private float cooldown;
     public GameObject Thrust;
@@ -41,82 +42,85 @@ public class PlayerMovement : MonoBehaviour
     {
         rb2D = GetComponent<Rigidbody2D>();
         rb2D.gravityScale = 0;
-
+        Invoke("WaitForReady", 0.4f);
     }
 
     private void Update()
     {
-        if (isFiring == false)
+        if (!startup)
         {
-            //Sets and constantly updates to the basic movement speed
-            x = Input.GetAxis("Horizontal") * xspeed;
-            y = Input.GetAxis("Vertical") * yspeed;
-        }
-        else
-        {
-            //Sets and constantly updates to the strafing-mode movement speed
-            x = Input.GetAxis("Horizontal") * xspeedfiring;
-            y = Input.GetAxis("Vertical") * yspeedfiring;
-        }
-
-        //Shooting and rate-of-fire management
-        if (Input.GetButton("Fire1") && Time.time > cooldown)
-        {
-            cooldown = Time.time + fireRate;
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-        }
-
-        //Activates a boost effect on the ship whenever the player moves forward while not shooting
-        if (x > 0 && isFiring == false)
-        {
-            Thrust.SetActive(true);
-            //isBoosting = true;
-            if (violentThrust >= 0)
+            if (isFiring == false)
             {
-                violentThrust -= Time.deltaTime;
+                //Sets and constantly updates to the basic movement speed
+                x = Input.GetAxis("Horizontal") * xspeed;
+                y = Input.GetAxis("Vertical") * yspeed;
             }
             else
             {
-                Thrust.transform.Rotate(180,0,0);
-                violentThrust = 0.1f;
+                //Sets and constantly updates to the strafing-mode movement speed
+                x = Input.GetAxis("Horizontal") * xspeedfiring;
+                y = Input.GetAxis("Vertical") * yspeedfiring;
             }
-        }
-        else
-        {
-            Thrust.SetActive(false);
-            //isBoosting = false;
-        }
 
-        //Activates a backwards-boost effect on the ship whenever the player moves in reverse while not shooting
-        if (x < 0 && isFiring == false)
-        {
-            Reverse.SetActive(true);
-            //isReversing = true;
-            if (violentThrust >= 0)
+            //Shooting and rate-of-fire management
+            if (Input.GetButton("Fire1") && Time.time > cooldown)
             {
-                violentThrust -= Time.deltaTime;
+                cooldown = Time.time + fireRate;
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            }
+
+            //Activates a boost effect on the ship whenever the player moves forward while not shooting
+            if (x > 0 && isFiring == false)
+            {
+                Thrust.SetActive(true);
+                //isBoosting = true;
+                if (violentThrust >= 0)
+                {
+                    violentThrust -= Time.deltaTime;
+                }
+                else
+                {
+                    Thrust.transform.Rotate(180, 0, 0);
+                    violentThrust = 0.1f;
+                }
             }
             else
             {
-                violentThrust = 0.1f;
+                Thrust.SetActive(false);
+                //isBoosting = false;
             }
-        }
-        else
-        {
-            Reverse.SetActive(false);
-            //isReversing = false;
-        }
 
-        
+            //Activates a backwards-boost effect on the ship whenever the player moves in reverse while not shooting
+            if (x < 0 && isFiring == false)
+            {
+                Reverse.SetActive(true);
+                //isReversing = true;
+                if (violentThrust >= 0)
+                {
+                    violentThrust -= Time.deltaTime;
+                }
+                else
+                {
+                    violentThrust = 0.1f;
+                }
+            }
+            else
+            {
+                Reverse.SetActive(false);
+                //isReversing = false;
+            }
 
-        //Checks if the player is holding down the shoot button, to change a variable affecting the player movement.
-        if (Input.GetButton("Fire1"))
-        {
-            isFiring = true;
-        }
-        else
-        {
-            isFiring = false;
+
+
+            //Checks if the player is holding down the shoot button, to change a variable affecting the player movement.
+            if (Input.GetButton("Fire1"))
+            {
+                isFiring = true;
+            }
+            else
+            {
+                isFiring = false;
+            }
         }
     }
 
@@ -124,6 +128,11 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb2D.velocity = new Vector2(x, y);
+    }
+
+    void WaitForReady()
+    {
+        startup = false;
     }
 }
 
